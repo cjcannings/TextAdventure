89b046d05ccc515e5ccf88112d71302b20687023
Christopher <christopher1803cj@gmail.com>
2021-09-03 22:00:58 +0100

    Fixed extra space in string

--- rooms.py
+++ rooms.py
@@ -1,7 +1,7 @@
 scenes = {
     1:
     {
-        'description': 'This is the starter room.\n There is a door to your left, staircase to your right, and a large pit ahead of you.',
+        'description': 'This is the starter room.\nThere is a door to your left, staircase to your right, and a large pit ahead of you.',
         'actions': {'Jump in the pit': 2, 'Walk through the door': 3, 'Go upstairs': 4}
     },
 
