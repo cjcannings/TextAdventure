4468c8da7d003c501bab4578b65185c07ba8093c
Christopher <christopher1803cj@gmail.com>
2021-09-06 13:30:06 +0100

    Finished fight logic

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from rooms import scenes
 from characters import enemies
 
+import random
+
 # TODO add inventory system to manage items held by player
 
 # Hero class for tracking of player properties/attributes
@@ -11,23 +13,49 @@ class Hero:
         self.name = name
 
     # function for fighting enemies
-    def fight(self, currentRoom, nextRoom):
+    def fight(self, currentRoom):
         enemyId = currentRoom.get('enemy')
         enemy = enemies.get(enemyId)
         enemyName = enemy.get('name')
         enemyHealth = enemy.get('health')
         enemyDamage = enemy.get('damage')
-        print(f'You attacked the {enemyName}!')
+
+        while enemyHealth > 0:
+            # damage dealt is within 20% of the enemy/hero's original damage, allowing same variation
+            enemyDealt = random.randint(round(enemyDamage * 0.8), round(enemyDamage * 1.2))
+            heroDealt = random.randint(round(self.damage * 0.8), round(self.damage * 1.2))
+
+            enemyHealth -= heroDealt
+            print(f'You dealt {heroDealt} damage!')
+            # perform check to ensure enemy does not deal damage after health is already depleted
+            if enemyHealth <= 0:
+                break
+            self.health -= enemyDealt
+            if self.health <= 0:
+                endGame()
+            print(f'Ouch! You took {enemyDealt} damage!')
+
+        print(f'You killed the {enemyName}! You have {self.health} health remaining.')
+
+        nextRoomNum = currentRoom.get('next')
+        nextRoom = scenes.get(nextRoomNum)
+        loadScene(nextRoom, self)
+
         return
 
 # game starter function
 def startGame():
     heroName = input('Welcome to the game! What is your name?')
+    print(f'Hello there, {heroName}!')
     hero = Hero(10, 2, heroName)
 
     currentRoom = scenes.get(1)
     loadScene(currentRoom, hero)
 
+
+# TODO
+def endGame():
+    return
 
 # main logic for game
 def loadScene(currentRoom, hero):
@@ -51,7 +79,7 @@ def loadScene(currentRoom, hero):
                     currentRoom = previousRoom
 
                 elif nextRoom == 'fight':
-                    hero.fight(currentRoom, nextRoom)
+                    hero.fight(currentRoom)
 
                 else:
                     previousRoom = currentRoom
